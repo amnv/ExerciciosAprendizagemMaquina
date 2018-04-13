e8a9f8c94d15adba0c0c149497d3265c0e5e8950
Allyson Manoel <amnv@cin.ufpe.br>
2018-04-13 00:38:43 -0300

    lqv1 finalizado

--- exercicio2/lqv1.py
+++ exercicio2/lqv1.py
@@ -6,45 +6,52 @@ from sklearn.neighbors import KNeighborsClassifier
 """Works using ndArray from numpy
     Avoid using regular python vector"""
 class Lqv1:
-
     @staticmethod
     def get_best_class(prototype_set, prototype_label, pattern):
         """Prototype must be an ndarray"""
+        print("find best class")
         knn = KNeighborsClassifier(n_neighbors=1)
-        knn.fit(prototype_set, prototype_label)
-        return knn.predict(pattern), knn.kneighbors(pattern)
+        p_label = prototype_label.reshape(-1, 1)
+        knn.fit(prototype_set, prototype_label.ravel())
+        pat = pattern.values.reshape(1, -1)
+        return knn.predict(pat), knn.kneighbors(pat, return_distance=False)[0][0]
 
     @staticmethod
-    def test_prototype(prototype_set, prototype_label, test_set):
-        knn = KNeighborsClassifier(n_neighbors=1)
+    def test_prototype(prototype_set, prototype_label, test_set, test_label, k=1):
+        knn = KNeighborsClassifier(n_neighbors=k)
         knn.fit(prototype_set, prototype_label)
-        return knn.score(test_set)
+        return knn.score(test_set, test_label)
 
     @staticmethod
     def build_prototypes(qnt_features, size, labels_list):
         labels = []
-        feature_ndarray = np.array([])
-        for i in range(labels_list):
+        feature_ndarray = np.array([], np.float64)
+        for i in range(size):
             feature = []
             for j in range(qnt_features):
                 feature.append(random.random())
             labels.append(random.choice(labels_list))
-            np.concatenate((feature_ndarray, feature))
+            feature_ndarray = np.concatenate((feature_ndarray, np.array(feature, np.float64)))
 
-        return feature_ndarray, np.array(labels)
-
-    @staticmethod
-    def adjust_learning_rate(learning_rate):
-        return 0
+        return feature_ndarray.reshape((size, qnt_features)), np.array(labels)
 
     @staticmethod
-    def train(train_set, prototype_set, prototype_label, iterations=200, learning_rate=0.001, threshold=0.10):
+    def adjust_learning_rate(learning_rate, is_correct_classified):
+        s = 1 if is_correct_classified else -1
+        d = (1 + s * learning_rate)
+        if d == 0:
+            return 0
+        return learning_rate/d
+
+    @staticmethod
+    def train(train_set, train_label, prototype_set, prototype_label, iterations=200, learning_rate=0.001, threshold=0.10):
         err = 1
-        while iterations > 1 or err > threshold:
-            for item in train_set:
-                c, prototype_position = Lqv1.get_best_class(prototype_set, prototype_label, item)
-
-                if c == item.getClass():
+        while iterations > 1 and err > threshold:
+            for i in range(train_set.shape[0]):
+                c, prototype_position = Lqv1.get_best_class(prototype_set, prototype_label, train_set.loc[i])
+                is_correct_classified = c == train_label.loc[i].item()
+                print("is_correct_classified " , is_correct_classified)
+                if is_correct_classified:
                     # Approximate prototype
                     # ei + α(t) × [x − ei]
                     prototype_set[prototype_position] += learning_rate * (c - prototype_set[prototype_position])
@@ -54,11 +61,12 @@ class Lqv1:
                     prototype_set[prototype_position] -= learning_rate * (c - prototype_set[prototype_position])
 
             # Exponentially decreasing
-            learning_rate = Lqv1.adjust_learning_rate(learning_rate)
-
+            learning_rate = Lqv1.adjust_learning_rate(learning_rate, is_correct_classified)
+            print("learning_rate " + str(learning_rate))
             # Reduce iterations
             iterations -= 1
 
             # Testing prototype efficiency
-            err = 1 - Lqv1.test_prototype(prototype_set, prototype_label, train_set)
-
+            err = 1 - Lqv1.test_prototype(prototype_set, prototype_label, train_set, train_label)
+            print("err " + str(err))
+        return prototype_set
--- exercicio2/main.py
+++ exercicio2/main.py
@@ -2,11 +2,18 @@ import pandas as pd
 from lqv1 import Lqv1
 
 #base e dados da base
-texto = pd.read_csv("base1.csv", names=["loc", "v(g)", "ev(g)",\
+texto = pd.read_csv("../base2.csv", names=["loc", "v(g)", "ev(g)",\
                                       "iv(g)", "n", "v", "l", "d","i", "e", "b", "t", "lOCode",\
                                       "loComment", "lOBlank", "locCodeAndComment", "uniq_Op", "uniq_Opnd", \
                                       "total_Op", "total_Opnd", "branchCount", "defects"])
 
-classes = texto[:len(texto.columns) - 1]
-lables = texto[-1]
+classes = texto.loc[:, texto.columns[:texto.columns.size - 1]]
+lables = texto.iloc[:, [texto.columns.size - 1]]
 
+tam = int(classes.shape[0]/2)
+
+prototype_set, prototype_label = Lqv1.build_prototypes(texto.columns.size - 1, 10, texto.defects.unique())
+train = Lqv1.train(classes[:tam], lables[:tam], prototype_set, prototype_label, 20, 1, 0.0010)
+score = Lqv1.test_prototype(train, prototype_label, classes[tam:], lables[tam:], 1)
+
+print("score " + str(score))
